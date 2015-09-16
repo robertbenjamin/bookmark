f6ee344257db736bef53a69b2bb6afd44a919b7b
Robert <i.am.robert.benjamin@gmail.com>
2015-09-16 16:41:05 -0400

    Add separate search controller

--- js/controllers/books.js
+++ js/controllers/books.js
@@ -15,30 +15,6 @@ angular
 			vm.books.$add(book)
 			vm.book = ""
 		}
-		
-		 // Test code for searching from the Google Books API
-
-		var search = $(".search");
-		var submit = $(".submit");
-		var searchResults = $(".search-results")
-
-		submit.click(function() {
-			searchResults.empty()
-			var query = search.val().split(" ").join("+")
-
-			$.getJSON("https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=AIzaSyDLhOKFqmBvr4XXHfoJfQFGevJR7cRekzQ" , function(data) {
-
-				for(i = 0; i < 4; i++) {
-					var currentItem = data.items[i].volumeInfo
-					searchResults.append( "<h2>Result</h2>")
-					searchResults.append( "<p>" + currentItem.title + " by " + currentItem.authors[0] + "</p>" )
-					// searchResults.append( "<p>" + currentItem.authors[0] + "</p>" )
-					// searchResults.append( "<img src='" + currentItem.imageLinks.thumbnail + "' ng-click='vm.add(vm.book)'>" )
-				}
-			})
-
-			console.log(query)
-		})
 
 	})
 
